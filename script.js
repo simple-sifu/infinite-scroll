c8788fa5a24f2b8b148acf2a9b7d9923dad94f93
Tommy Han <tommy_han1@intuit.com>
2021-12-28 02:25:19 -0500

    add array

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('hello World');
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
+// Unsplash API
+const count=10;
+const apiKey = '-vAmadi7qTiEaGCL83ldjmPbBV6i_xJkLT807Ti1_wM';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        console.log("photosArray=", photosArray);
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getPhotos();
